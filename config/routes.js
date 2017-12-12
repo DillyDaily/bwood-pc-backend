fb1e215b57c2af884cf2ce57c1fa89fc6b3fe0a1
Alexis <ali.finney@gmail.com>
2017-12-12 13:35:43 -0700

    changed some routes to accommodate admin side

--- config/routes.js
+++ config/routes.js
@@ -7,8 +7,13 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
   });
 
-//Get All Blog Posts
+//USERS - Get All Blog Posts
 router.get('/blog', function (req, res, next) {
+  knex('posts').select().then(post => res.json(post))
+});
+
+//ADMIN - Get All Blog Posts
+router.get('/admin/blog', function (req, res, next) {
   knex('posts').select().then(post => res.json(post))
 });
 
@@ -18,21 +23,21 @@ router.get('/blog/:id', function (req, res) {
 });
 
 //Add One Blog Post
-router.post('admin/blog/new', function (req, res) {
+router.post('/admin/blog/new', function (req, res) {
   knex('posts').insert(req.body).then(() => {
     knex('posts').select().then(posts => res.json(posts))
   });
 });
 
 //Edit One Blog Post
-router.patch('admin/blog/:id', function (req, res) {
+router.patch('/admin/blog/edit/:id', function (req, res) {
   knex('posts').update(req.body).where('id', req.params.id).then(function () {
     knex('posts').select().then(posts => res.json(posts))
   });
 });
 
 //Delete One Blog Post
-router.delete('admin/blog/:id', function (req, res) {
+router.delete('/delete/blog/:id', function (req, res) {
   knex('posts').del().where('id', req.params.id).then(function () {
     knex('posts').select().then(posts => res.json(posts))
   });
